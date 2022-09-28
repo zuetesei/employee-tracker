57bde628193ea7505124e3082beb14aa4b641cb8
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-09-28 14:36:46 -0700

    connect to mysql database

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express midlleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connection to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Boostbelau1701',
+        database: 'tracker'
+    },
+    console.log('Connected to the election database.')
+);
 
 // default response for any other request (not found)
 app.use((req, res) => {
